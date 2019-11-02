d5b1f72e842997ee9d0ddfa3bef9e741f190b9f9
nico <nicolas.perrel@gmail.com>
2019-11-02 16:49:12 +0100

    meta.py : on ignore les champs débutant par "ID/id ..."

--- metadata/meta.py
+++ metadata/meta.py
@@ -23,9 +23,9 @@ Structure du dictionnaire produit (exemple):
 r = {
     "test.shp": {
         "champs": {
-            "IDNUM": {
-                "datatype": "int:18",
-                "values": [0, 1, 2, 3, 4]
+            "profondeur": {
+                "datatype": "float:11.2",
+                "values": [45.21, 50.0, 45.0]
             },
             "position": {
                 "datatype": "str:254",
@@ -39,8 +39,11 @@ r = {
 
 vector_formats = [".shp", ".gpkg", ".geojson", ".tab", ".kml", ".gml", ".dxf"]
 results = {}
+ignored_field = []
 
+# pour chaque fichier dans le dossier courant ...
 for f in os.listdir(os.getcwd()):
+    # si le fichier porte l'extension d'un format vectoriel connu ...
     if os.path.splitext(f)[1].lower() in vector_formats:
         try:
             with fiona.open(f) as src:
@@ -48,19 +51,29 @@ for f in os.listdir(os.getcwd()):
                     print(f, "ne contient aucune information attributaire")
                 geom_type = src.meta["schema"]["geometry"]
                 fields = src.meta["schema"]["properties"]
+                # création de l'entrée dans le dictionnaire results (nom fichier, geom type)
                 results[f] = {"geom_type": geom_type, "champs": {}}
                 for name, datatype in fields.items():
-                    results[f]["champs"][name] = {"datatype": datatype, "values": []}
+                    # on exclu les champs commençants par id / ID / Id ...
+                    if "id" not in name[0:2].lower():
+                        # pour chaque champs, écriture de son nom et de son type ...
+                        results[f]["champs"][name] = {"datatype": datatype, "values": []}
+                    else:
+                        ignored_field.append(name)
 
                 for key, feat in src.items():
                     for field in fields:
-                        value = feat["properties"][field]
-                        if not value in results[f]["champs"][field]["values"]:
-                            results[f]["champs"][field]["values"].append(value)
+                        if field not in ignored_field:
+                            value = feat["properties"][field]
+                            # on inscrit la valeur du champs si elle n'est pas déjà enregistrée
+                            if not value in results[f]["champs"][field]["values"]:
+                                results[f]["champs"][field]["values"].append(value)
         except fiona.errors.DriverError as err_fiona_open:
+            print("Erreur à l'ouverture de", f)
             print(err_fiona_open)
             pass
 
+# écriture du dictionnaire results dans metadata.csv
 with open("metadata.csv", encoding="utf-8", newline="", mode="w") as csv_f:
     output = csv.writer(csv_f, delimiter=",")
     output.writerow(["FICHIER", "GEOM_TYPE", "CHAMP", "TYPE", "VALEUR"])
